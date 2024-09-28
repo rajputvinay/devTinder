b408bf07462f786a74a49f9b3db3a1f8daea6acd
rajputvinay <dev.vinay.rajput@gmail.com>
2024-09-28 11:42:56 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -3,22 +3,21 @@ const express = require("express")
 const app = express()//creating new express js application
 
 //Request Handler
-app.get("/user", (req, res) => {
-    res.send({firstname:"Vinay",lastname:"Rajput"})
-})
-app.post("/user", (req, res) => {
-    res.send("Data Added Successfully!!");
-})
-app.use("/test/34", (req, res) => {
-    res.send("Hello from the /test34")
-})
-app.use("/test", (req, res) => {
-    res.send("Hello from the /test")
-})
+app.use("/user", [(req, res, next) => {
+    // res.send("Hello from the / ")
+    console.log("hello");
+    next()
+    res.send("Response1")
+}, [(req, res, next) => {
+    console.log("res2");
+    next()
+    res.send("Response2")
+}], (req, res, next) => {
+    console.log("res3");
+    res.send("Response3")
 
-app.use("/", (req, res) => {
-    res.send("Hello from the / ")
-})
+}])
+
 app.listen(7717, () => {
     console.log("Server is successfully listening on port 7717...");
 
